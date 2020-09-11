a43bf053d07d97445aa2442ad5866510be16fcaa
Carlos Riquelme <criquelme2@gmail.com>
2020-09-11 19:27:07 -0300

    auth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="home">
     <Login></Login>
-    <vk-offcanvas-content>
-      <vk-button @click="show = !show" class="uk-margin-small-right"
-        >Open</vk-button
-      >
-      <a @click="show = !show">Open</a>
-
-      <vk-offcanvas :show.sync="show">
-        <vk-offcanvas-close @click="show = false"></vk-offcanvas-close>
-        <h3>Title</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </vk-offcanvas>
-    </vk-offcanvas-content>
   </div>
 </template>
 
@@ -25,13 +8,7 @@
 import Login from "@/components/Login.vue";
 
 export default {
-  name: "home",
-  data() {
-    return {
-      show: false,
-    };
-  },
-
+  name: "login",
   components: {
     Login,
   },
