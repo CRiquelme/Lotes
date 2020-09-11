003825da18527235d5145b26e86ef5cdf3e9f1d7
Dario Arrieta <arrieta.dario@hotmail.com>
2020-09-11 10:02:32 -0500

    router y componente login

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+ <template>
+   <div>
+       <h2>Ingresar</h2> 
+       <form>
+           <input type="email" placeholder="Correo">
+           <input type="password" placeholder="Contrasena">
+           <input type="submit" value="Ingresar">
+       </form>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
