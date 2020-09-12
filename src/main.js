93a770f193cfaf3dcd3212bbd4c77dc09f52d5b0
Carlos Riquelme <criquelme2@gmail.com>
2020-09-12 20:53:52 -0300

    validate register form

--- src/main.js
+++ src/main.js
@@ -5,16 +5,17 @@ import "./assets/styles/index.css";
 import Vuikit from "vuikit";
 import VuikitIcons from "@vuikit/icons";
 import "@vuikit/theme";
-import firebase from 'firebase'
+import firebase from "firebase";
+import Vuelidate from "vuelidate";
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuikit);
 Vue.use(VuikitIcons);
-firebase.auth().onAuthStateChanged(()=>{
+Vue.use(Vuelidate);
+firebase.auth().onAuthStateChanged(() => {
   new Vue({
     router,
     render: (h) => h(App),
   }).$mount("#app");
-})
-
+});
--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -8,86 +8,197 @@
       class="w-full max-w-lg text-left bg-gray-100 shadow-md rounded px-10 pt-10 pb-10 mb-4"
     >
       <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <!-- Nombre -->
+        <div class="form-group w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            :class="{
+              'label-error': $v.nombre.$error,
+              label: !$v.nombre.$error,
+            }"
             for="nombre"
           >
             Nombre
           </label>
           <input
-            class="appearance-none block w-full bg-white text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            :class="{
+              'input-error': $v.nombre.$error,
+              input: !$v.nombre.$error,
+            }"
             id="nombre"
             type="text"
             placeholder="Nombre"
-            v-model="nombre"
+            v-model.trim="$v.nombre.$model"
           />
-          <!-- <p class="text-red-500 text-xs italic">ejemplo de error</p> -->
+          <div
+            v-if="!$v.nombre.required"
+            :class="{
+              'mensaje-error': $v.nombre.$error,
+              hidden: !$v.nombre.$error,
+            }"
+          >
+            Es requerido
+          </div>
+          <div
+            :class="{ 'mensaje-error': $v.nombre.$error }"
+            v-if="!$v.nombre.minLength"
+          >
+            Debe contener al menos
+            {{ $v.nombre.$params.minLength.min }} letras.
+          </div>
         </div>
+        <!-- apellido -->
         <div class="w-full md:w-1/2 px-3">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            :class="{
+              'label-error': $v.apellido.$error,
+              label: !$v.apellido.$error,
+            }"
             for="apellido"
           >
             Apellido
           </label>
           <input
-            class="appearance-none block w-full bg-white text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            :class="{
+              'input-error': $v.apellido.$error,
+              input: !$v.apellido.$error,
+            }"
+            v-model.trim="$v.apellido.$model"
             id="apellido"
             type="text"
             placeholder="Apellido"
-            v-model="apellido"
           />
+          <div
+            v-if="!$v.apellido.required"
+            :class="{
+              'mensaje-error': $v.apellido.$error,
+              hidden: !$v.apellido.$error,
+            }"
+          >
+            Es requerido
+          </div>
+          <div
+            :class="{ 'mensaje-error': $v.apellido.$error }"
+            v-if="!$v.apellido.minLength"
+          >
+            Debe contener al menos
+            {{ $v.apellido.$params.minLength.min }} letras.
+          </div>
         </div>
       </div>
 
+      <!-- email -->
       <div class="mb-4">
         <label
-          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          :class="{
+            'label-error': $v.email.$error,
+            label: !$v.email.$error,
+          }"
           for="email"
         >
           Correo electrónico
         </label>
         <input
-          class="appearance-none block w-full bg-white text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          :class="{
+            'input-error': $v.email.$error,
+            input: !$v.email.$error,
+          }"
+          v-model.trim="$v.email.$model"
           id="email"
           type="text"
           placeholder="Correo electrónico"
-          v-model="email"
         />
+        <div
+          v-if="!$v.email.required"
+          :class="{
+            'mensaje-error': $v.email.$error,
+            hidden: !$v.email.$error,
+          }"
+        >
+          Es requerido
+        </div>
+        <div
+          v-if="!$v.email.email"
+          :class="{
+            'mensaje-error': $v.email.$error,
+            hidden: !$v.email.$error,
+          }"
+        >
+          Debe ser un correo electrónico válido
+        </div>
       </div>
+
+      <!-- contraseña -->
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            :class="{
+              'label-error': $v.contrasena.$error,
+              label: !$v.contrasena.$error,
+            }"
             for="contrasena"
           >
             Contraseña
           </label>
           <input
-            class="appearance-none block w-full bg-white text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            :class="{
+              'input-error': $v.contrasena.$error,
+              input: !$v.contrasena.$error,
+            }"
+            v-model.trim="$v.contrasena.$model"
             id="contrasena"
             type="password"
             placeholder="******************"
-            v-model="contrasena"
           />
+          <div
+            v-if="!$v.contrasena.required"
+            :class="{
+              'mensaje-error': $v.contrasena.$error,
+              hidden: !$v.contrasena.$error,
+            }"
+          >
+            Es requerido
+          </div>
+          <div
+            :class="{ 'mensaje-error': $v.contrasena.$error }"
+            v-if="!$v.contrasena.minLength"
+          >
+            Debe contener al menos
+            {{ $v.contrasena.$params.minLength.min }} letras.
+          </div>
         </div>
+
+        <!-- confirmación contraseña -->
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            :class="{
+              'label-error': $v.confirmacion_contrasena.$error,
+              label: !$v.confirmacion_contrasena.$error,
+            }"
             for="confirmar_contrasena"
           >
             Confirmar contraseña
           </label>
           <input
-            class="appearance-none block w-full bg-white text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="confirmar_contrasena"
+            :class="{
+              'input-error': $v.confirmacion_contrasena.$error,
+              input: !$v.confirmacion_contrasena.$error,
+            }"
+            v-model.trim="$v.confirmacion_contrasena.$model"
+            id="confirmacion_contrasena"
             type="password"
             placeholder="******************"
-            v-model="confirmar_contrasena"
           />
+          <div
+            :class="{
+              'mensaje-error': !$v.confirmacion_contrasena.sameAsPassword,
+            }"
+            v-if="!$v.confirmacion_contrasena.sameAsPassword"
+          >
+            Contraseñas deben ser identicas
+          </div>
         </div>
       </div>
+
       <div class="text-center text-xl">
         <input
           type="submit"
@@ -102,30 +213,28 @@
 <script>
 import { db, f } from "../firebase";
 import "firebase/auth";
+import { required, minLength, sameAs, email } from "vuelidate/lib/validators";
 
 export default {
   name: "Registro",
   data() {
     return {
+      errors: [],
       email: "",
       contrasena: "",
-      confirmar_contrasena: "",
+      confirmacion_contrasena: "",
       nombre: "",
       apellido: "",
+      submitStatus: null,
     };
   },
   methods: {
     registro() {
       let self = this;
-      // Comprobar
-      if (
-        self.nombre != "" &&
-        self.apellido != "" &&
-        self.email != "" &&
-        self.contrasena != "" &&
-        self.contrasena != self.confirmar_contrasena
-      ) {
-        // Si pasa la comprobación
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = "ERROR";
+      } else {
         f.auth()
           .createUserWithEmailAndPassword(this.email, this.contrasena)
           .then(function(data) {
@@ -145,10 +254,28 @@ export default {
             console.log(errorCode);
             console.log(errorMessage);
           });
-      } else {
-        // Si hay errores
-        self.nombre === "" ? document.getElementById("myAnchor") : "";
       }
+    },
+  },
+  validations: {
+    nombre: {
+      required,
+      minLength: minLength(3),
+    },
+    apellido: {
+      required,
+      minLength: minLength(3),
+    },
+    contrasena: {
+      required,
+      minLength: minLength(6),
+    },
+    confirmacion_contrasena: {
+      sameAsPassword: sameAs("contrasena"),
+    },
+    email: {
+      email,
+      required,
     },
   },
 };
