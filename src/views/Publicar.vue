708575fce984d420644fafe67c23b7cd594d8707
Dario Arrieta <arrieta.dario@hotmail.com>
2020-09-30 15:24:38 -0500

    inicios de mapa de publicar

--- src/views/Publicar.vue
+++ src/views/Publicar.vue
@@ -10,8 +10,69 @@
     <!-- Contenido de pasos -->
     <div class="steps-content px-4 py-4">
       <!-- Paso 1 -->
-      <div v-show="steps[current].title === 'Paso 1'">
-        <!-- Darío, este div es para que agregues la primera parte del formulario. Puedes eliminar este comentario -->
+      <div v-show="steps[current].title === 'Paso 1'" 
+      class="flex flex-row flex-wrap"
+      >
+        <!-- Listas -->
+        <div class="w-1/2 flex flex-col">
+            <span>
+                Provincia:
+                <select 
+                    name="Provincia"
+                    id="selectPrv"
+                    @change="ProvinciaChange">
+                      <option 
+                          v-for="pr in provincias" 
+                          :key="pr.key"
+                          :value="pr.key" >{{pr.value}}</option>
+                </select>
+            </span>
+            <span>
+                Cantón:
+                <select 
+                    v-if="cantones"
+                    @change="CantonChange" 
+                    name="Cantones"
+                    id="selectCtn" >
+                    <option 
+                        v-for="ct in cantones" 
+                        :key="ct.key"
+                        :value="ct.key" >{{ct.value}}</option>
+                </select>
+            </span>
+            <span>
+                Distrito:
+                <select 
+                    v-if="distritos"
+                    @change="DistritoChange" 
+                    name="Distritos"
+                    id="selectDtt" >
+                    
+                    <option 
+                        v-for="dt in distritos" 
+                        :key="dt.key"
+                        :value="dt.key" >{{dt.value}}</option>
+                </select>
+            </span>
+
+        </div>
+        <!-- Mapa -->
+        <div class="w-1/2">
+          <GmapMap
+              :center="selectedCenter"
+              :zoom="selectedZoom"
+              mapTypeControl= "false"
+              streetViewControl= "false"
+              class="w-full h-64"
+              ref='mapRef'>
+                <GmapMarker
+                  :position="selectedCenter"
+                  :draggable="true"
+                  @dragend="coordenadasManuales()"
+                  ref="GoogleMrkr"
+                />
+          </GmapMap>
+        </div>
       </div>
 
       <!-- Paso 2 -->
@@ -195,194 +256,311 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      current: 0,
-      steps: [
-        {
-          title: "Paso 1",
+  import Vue from "vue";
+  export default {
+    data() {
+      return {
+        current: 0,
+        steps: [
+          {
+            title: "Paso 1",
+          },
+          {
+            title: "Paso 2",
+          },
+          {
+            title: "Paso 3",
+          },
+        ],
+        area_terreno: "",
+        medida_frente_terreno: "",
+        tipo_medida: "",
+        contaran_clientes: [],
+        frente_terreno_acceso: "",
+        frente_terreno_acceso_id: "",
+        nivel_terreno: "",
+        contaran_clientes_options: [
+          "Acera",
+          "Cordón y caño",
+          "Alumbrado público",
+          "Teléfono",
+          "Electricidad",
+          "Cañería de agua potable",
+          "Cobertura celular",
+          "Cable TV",
+          "Fibra optica",
+          "Internet",
+          "Alcantarillado sanitario",
+          "Aseo y recolección basura",
+          "Facilidades comunales",
+          "Edificios comerciales",
+          "Escuela primaria",
+          "Educación secundaria",
+          "Tempo católico y otras",
+          "Servicios privados, municipales, comerciales y financieros",
+          "Servicios de salud",
+          "Áreas recreativas",
+          "Planta de tratamiento aguas residuales cercanas",
+          "Riesgo de inundación o deslizamiento",
+          "Afectado por alta tensión",
+          "Transporte público (frente a vía publica)",
+        ],
+        marcadores: [
+          { id: 0, estilo: "left: 30%; top: 22%" },
+          { id: 1, estilo: "left: 49%; top: 29%" },
+          { id: 2, estilo: "left: 29%; top: 45%" },
+          { id: 3, estilo: "left: 36%; top: 45%" },
+          { id: 4, estilo: "left: 48%; top: 42%" },
+          { id: 5, estilo: "left: 37%; top: 57%" },
+          { id: 6, estilo: "left: 40%; top: 68%" },
+          { id: 7, estilo: "left: 72%; top: 58%" },
+        ],
+        imagenesTopografia: [
+          { valor: "plano-0-a-5", img: "/images/plano-0-a-5.png" },
+          {
+            valor: "plana_ondulada_de_5_a_15",
+            img: "/images/plana_ondulada_de_5_a_15.png",
+          },
+          {
+            valor: "accidentada_de_15_a_30",
+            img: "/images/accidentada_de_15_a_30.png",
+          },
+          {
+            valor: "muy_accidentada_de_30_a_45",
+            img: "/images/muy_accidentada_de_30_a_45.png",
+          },
+          {
+            valor: "quebrada_de_45_a_60",
+            img: "/images/quebrada_de_45_a_60.png",
+          },
+          {
+            valor: "muy_quebrada_mayor_a_60",
+            img: "/images/muy_quebrada_mayor_a_60.png",
+          },
+        ],
+        previewVisible: false,
+        previewImage: "",
+        fileList: [],
+        headers: {
+          authorization: "authorization-text",
         },
-        {
-          title: "Paso 2",
-        },
-        {
-          title: "Paso 3",
-        },
-      ],
-      area_terreno: "",
-      medida_frente_terreno: "",
-      tipo_medida: "",
-      contaran_clientes: [],
-      frente_terreno_acceso: "",
-      frente_terreno_acceso_id: "",
-      nivel_terreno: "",
-      contaran_clientes_options: [
-        "Acera",
-        "Cordón y caño",
-        "Alumbrado público",
-        "Teléfono",
-        "Electricidad",
-        "Cañería de agua potable",
-        "Cobertura celular",
-        "Cable TV",
-        "Fibra optica",
-        "Internet",
-        "Alcantarillado sanitario",
-        "Aseo y recolección basura",
-        "Facilidades comunales",
-        "Edificios comerciales",
-        "Escuela primaria",
-        "Educación secundaria",
-        "Tempo católico y otras",
-        "Servicios privados, municipales, comerciales y financieros",
-        "Servicios de salud",
-        "Áreas recreativas",
-        "Planta de tratamiento aguas residuales cercanas",
-        "Riesgo de inundación o deslizamiento",
-        "Afectado por alta tensión",
-        "Transporte público (frente a vía publica)",
-      ],
-      marcadores: [
-        { id: 0, estilo: "left: 30%; top: 22%" },
-        { id: 1, estilo: "left: 49%; top: 29%" },
-        { id: 2, estilo: "left: 29%; top: 45%" },
-        { id: 3, estilo: "left: 36%; top: 45%" },
-        { id: 4, estilo: "left: 48%; top: 42%" },
-        { id: 5, estilo: "left: 37%; top: 57%" },
-        { id: 6, estilo: "left: 40%; top: 68%" },
-        { id: 7, estilo: "left: 72%; top: 58%" },
-      ],
-      imagenesTopografia: [
-        { valor: "plano-0-a-5", img: "/images/plano-0-a-5.png" },
-        {
-          valor: "plana_ondulada_de_5_a_15",
-          img: "/images/plana_ondulada_de_5_a_15.png",
-        },
-        {
-          valor: "accidentada_de_15_a_30",
-          img: "/images/accidentada_de_15_a_30.png",
-        },
-        {
-          valor: "muy_accidentada_de_30_a_45",
-          img: "/images/muy_accidentada_de_30_a_45.png",
-        },
-        {
-          valor: "quebrada_de_45_a_60",
-          img: "/images/quebrada_de_45_a_60.png",
-        },
-        {
-          valor: "muy_quebrada_mayor_a_60",
-          img: "/images/muy_quebrada_mayor_a_60.png",
-        },
-      ],
-      previewVisible: false,
-      previewImage: "",
-      fileList: [],
-      headers: {
-        authorization: "authorization-text",
+        topografia: "",
+        archivos: [
+          { titulo: "Plano de catastro", id: "file_plano_de_catastro" },
+          { titulo: "Uso de suelo", id: "file_uso_de_suelo" },
+          { titulo: "Disponibilidad de agua", id: "file_disponibilidad_de_agua" },
+          {
+            titulo: "Disponibilidad eléctrica",
+            id: "file_disponibilidad_electrica",
+          },
+          {
+            titulo: "Visado municipal",
+            id: "file_visado_municipal",
+          },
+        ],
+        nivelCalle: [
+          { titulo: "Bajo nivel de calle" },
+          { titulo: "Sobre nivel de la calle" },
+          { titulo: "A nivel de la calle" },
+        ],
+        tipoMedida: [{ titulo: "Metros cuadrados" }, { titulo: "Héctareas" }],
+
+        provincias:[],
+        cantones:[],
+        distritos:[],
+
+        selectedCenter:{lat:10,lng:-84},
+        selectedZoom:7
+      };
+    },
+    methods: {
+      next() {
+        this.current++;
       },
-      topografia: "",
-      archivos: [
-        { titulo: "Plano de catastro", id: "file_plano_de_catastro" },
-        { titulo: "Uso de suelo", id: "file_uso_de_suelo" },
-        { titulo: "Disponibilidad de agua", id: "file_disponibilidad_de_agua" },
-        {
-          titulo: "Disponibilidad eléctrica",
-          id: "file_disponibilidad_electrica",
-        },
-        {
-          titulo: "Visado municipal",
-          id: "file_visado_municipal",
-        },
-      ],
-      nivelCalle: [
-        { titulo: "Bajo nivel de calle" },
-        { titulo: "Sobre nivel de la calle" },
-        { titulo: "A nivel de la calle" },
-      ],
-      tipoMedida: [{ titulo: "Metros cuadrados" }, { titulo: "Héctareas" }],
-    };
-  },
-  methods: {
-    next() {
-      this.current++;
+      prev() {
+        this.current--;
+      },
+      frenteTerreno(dato) {
+        var frente_de_terreno = [
+          "Lote esquinero",
+          "Lote cabecero",
+          "Lote medianero",
+          "Lote en callejón fondo",
+          "Lote callejón lateral",
+          "Lote medianero 2 frentes",
+          "Lote cabecero",
+          "Lote manzanero",
+        ];
+        this.frente_terreno_acceso_id = dato;
+        this.frente_terreno_acceso = frente_de_terreno[dato];
+      },
+
+
+      //Métodos paso 1
+      
+      ProvinciaChange(){
+        //recupera coordenadas
+        const selectedCity=document.getElementById('selectPrv').selectedOptions[0].outerText
+        var addressObj = {
+            address_line_1: '',
+            address_line_2: '',
+            city:selectedCity,
+            state:'',          
+            zip_code:'',     
+            country:'Costa Rica'
+        }
+        Vue.$geocoder.send(addressObj, x => {
+            this.selectedCenter={
+                lat:x.results[0].geometry.location.lat,
+                lng:x.results[0].geometry.location.lng
+            }
+        });
+
+        //llena lista desplegable de cantones
+        this.cantones=[]
+       this.distritos=[]
+        const link="https://ubicaciones.paginasweb.cr/provincia/" + document.getElementById('selectPrv').value + "/cantones.json"
+        
+        fetch(link)
+        .then(ctns=>ctns.json())
+        .then(ctn=>Object.entries(ctn).forEach(([key,value])=>(this.cantones.push({key,value}))))
+        this.selectedZoom=12
+      },
+      CantonChange(){
+        //recupera coordenadas
+        const selectedCanton=document.getElementById('selectCtn').selectedOptions[0].outerText
+        const selectedCity=document.getElementById('selectPrv').selectedOptions[0].outerText
+        var addressObj = {
+            city:selectedCity,
+            state:selectedCanton,          
+            zip_code:'',     
+            country:'Costa Rica'
+        }
+        Vue.$geocoder.send(addressObj, x => {
+            this.selectedCenter={
+                lat:x.results[0].geometry.location.lat,
+                lng:x.results[0].geometry.location.lng
+            }
+        })
+
+        //llena lista desplegable de Distritos
+        this.distritos=[]
+        const link="https://ubicaciones.paginasweb.cr/provincia/" + document.getElementById('selectPrv').value + "/canton/" + document.getElementById('selectCtn').value + "/distritos.json"
+        
+        fetch(link)
+        .then(dtrs=>dtrs.json())
+        .then(dtr=>Object.entries(dtr).forEach(([key,value])=>(this.distritos.push({key,value}))))
+
+        this.selectedZoom=12
+      },
+      DistritoChange(){
+          //recupera coordenadas
+          const selectedDistrito=document.getElementById('selectDtt').selectedOptions[0].outerText
+          const selectedCanton=document.getElementById('selectCtn').selectedOptions[0].outerText
+          const selectedCity=document.getElementById('selectPrv').selectedOptions[0].outerText
+          var addressObj = {
+              city:selectedCity,
+              state:selectedCanton,          
+              zip_code:selectedDistrito,     
+              country:'Costa Rica'
+          }
+          Vue.$geocoder.send(addressObj, x => {
+              this.selectedCenter={
+                  lat:x.results[0].geometry.location.lat,
+                  lng:x.results[0].geometry.location.lng
+              }
+          })
+          this.selectedZoom=15
+      },
+
+      coordenadasManuales()
+      {
+        console.log(this.selectedCenter.lat)
+      }
     },
-    prev() {
-      this.current--;
+    created () {
+        //provincias inicial
+        fetch('https://ubicaciones.paginasweb.cr/provincias.json')
+        .then(prvs=>prvs.json())
+        .then(ps=>Object.entries(ps).forEach(([key,value])=>(this.provincias.push({key,value}))))
+        .then( ()=>{
+          //cantones inicial
+          let link="https://ubicaciones.paginasweb.cr/provincia/" + document.getElementById('selectPrv').value + "/cantones.json"
+          
+          fetch(link)
+          .then(ctns=>ctns.json())
+          .then(ctn=>Object.entries(ctn).forEach(([key,value])=>(this.cantones.push({key,value}))))
+          .then(()=>{
+            //Distritos inicial
+            const link="https://ubicaciones.paginasweb.cr/provincia/" + document.getElementById('selectPrv').value + "/canton/" + document.getElementById('selectCtn').value + "/distritos.json"
+            
+            fetch(link)
+            .then(dtrs=>dtrs.json())
+            .then(dtr=>Object.entries(dtr).forEach(([key,value])=>(this.distritos.push({key,value}))))
+          } )
+        })
     },
-    frenteTerreno(dato) {
-      var frente_de_terreno = [
-        "Lote esquinero",
-        "Lote cabecero",
-        "Lote medianero",
-        "Lote en callejón fondo",
-        "Lote callejón lateral",
-        "Lote medianero 2 frentes",
-        "Lote cabecero",
-        "Lote manzanero",
-      ];
-      this.frente_terreno_acceso_id = dato;
-      this.frente_terreno_acceso = frente_de_terreno[dato];
-    },
-  },
-};
+    updated(){
+        this.$refs.mapRef.$mapPromise
+        .then((map) => this.map=map);
+    }
+  };
 </script>
 
 <style scoped>
-.steps-content {
-  margin-top: 16px;
-  border: 1px dashed #e9e9e9;
-  border-radius: 6px;
-  background-color: #fafafa;
-  min-height: 200px;
-}
+  .steps-content {
+    margin-top: 16px;
+    border: 1px dashed #e9e9e9;
+    border-radius: 6px;
+    background-color: #fafafa;
+    min-height: 200px;
+  }
 
-.steps-action {
-  margin-top: 24px;
-}
-.active_frente_terreno {
-  color: #2b0707 !important;
-  background-color: #eac111 !important;
-}
+  .steps-action {
+    margin-top: 24px;
+  }
+  .active_frente_terreno {
+    color: #2b0707 !important;
+    background-color: #eac111 !important;
+  }
 
-.ant-upload-select-picture-card i {
-  font-size: 32px;
-  color: #999;
-}
+  .ant-upload-select-picture-card i {
+    font-size: 32px;
+    color: #999;
+  }
 
-.ant-upload-select-picture-card .ant-upload-text {
-  margin-top: 8px;
-  color: #666;
-}
+  .ant-upload-select-picture-card .ant-upload-text {
+    margin-top: 8px;
+    color: #666;
+  }
 
-.hiddenradio [type="radio"] {
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-.hiddenradio [type="radio"] + img {
-  position: relative;
-  cursor: pointer;
-  filter: drop-shadow(0px 7px 7px #000);
-  max-height: 300px;
-  height: 100%;
-  width: 100%;
-  transform: scale(1);
-  transition: all 300ms ease;
-  z-index: 1;
-}
-.hiddenradio [type="radio"] + img:hover {
-  transform: scale(1.5);
-  filter: drop-shadow(1px 1px 5px rgb(105, 104, 104));
-  z-index: 3;
-  background-color: white;
-}
-.hiddenradio [type="radio"]:checked + img {
-  outline: 1px solid rgb(5, 56, 12);
-  transform: scale(1);
-  background-color: white;
-  filter: none;
-}
+  .hiddenradio [type="radio"] {
+    position: absolute;
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  .hiddenradio [type="radio"] + img {
+    position: relative;
+    cursor: pointer;
+    filter: drop-shadow(0px 7px 7px #000);
+    max-height: 300px;
+    height: 100%;
+    width: 100%;
+    transform: scale(1);
+    transition: all 300ms ease;
+    z-index: 1;
+  }
+  .hiddenradio [type="radio"] + img:hover {
+    transform: scale(1.5);
+    filter: drop-shadow(1px 1px 5px rgb(105, 104, 104));
+    z-index: 3;
+    background-color: white;
+  }
+  .hiddenradio [type="radio"]:checked + img {
+    outline: 1px solid rgb(5, 56, 12);
+    transform: scale(1);
+    background-color: white;
+    filter: none;
+  }
 </style>
