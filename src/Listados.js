0f7c223828d096bedd63713d98ab25b4976d6f0d
Dario Arrieta <arrieta.dario@hotmail.com>
2020-10-01 16:40:27 -0500

    parte de los listado de provincias y barrios agregada

--- src/Listados.js
+++ src/Listados.js
@@ -48,56 +48,56 @@ const PROVINCIAS=
                             nombre:"Catedral",
                             barrios:
                             [
-                                
+                                "Bella Vista", "California", "Carlos María Jiménez", "Catedral", "La Dolorosa", "Dos Pinos", "Francisco Peralta", "González Lahmann", "Güell", "La Cruz", "Laberinto", "Lomas de Ocloro", "Luján", "Mil Flor", "Naciones Unidas", "Pacífico (parte)", "San Cayetano", "Soledad", "Tabacalera", "Vasconia"
                             ]
                         },
                         {
                             nombre:"Zapote",
                             barrios:
                             [
-                                
+                                "Alborada", "Calderón Muñoz", "Cerrito", "Córdoba", "Lucía Jardín", "La Gloria", "Urbanización Las Luisas", "Los Mangos", "Montealegre", "Moreno Cañas", "Quesada Durán", "San Dimas", "San Gerardo", "Trébol", "Ujarrás", "Vista Hermosa", "Yoses Sur", "apote", "Barrio las Rosas"
                             ]
                         },
                         {
                             nombre:"San Francisco de Dos Ríos",
                             barrios:
                             [
-                                
+                                "Ahogados", "Cabañas", "Camelias", "Coopeguaria", "El Bosque", "El Faro", "Fátima", "El Hispano", "Lincoln", "Lomas de San Francisco", "Maalot", "Las Fresas (Barrios La Pacífica y Parque Méndez)", "San Francisco", "Los Sauces", "Los Saucitos", "Y Griega", "Zona Industrial", "Zurquí", "La Pacifica"
                             ]
                         },
                         {
                             nombre:"La Uruca",
                             barrios:
                             [
-                                
+                                "Alborada", "Bajos de Torres", "La Carpio", "Carranza", "Corazón de Jesús", "Cristal", "Electriona", "Finca de la Caja", "Florentino Castro", "Jardines de Autopista", "Las Ánimas", "Magnolias", "Marimil", "Monserrat", "Peregrina", "Robledal", "Rositer Carballo", "la brisas del virilla", "noche buena", "Santander", "Saturno", "Uruca Centro", "Vuelta del Virilla", "Carvajal Castro", "El Solar", "Zona Industrial"
                             ]
                         },
                         {
                             nombre:"Mata Redonda",
                             barrios:
                             [
-                                
+                                "Américas", "Anonos", "Balcón Verde", "Morenos", "Niza", "Del Pino", "Holanda", "La Salle", "La Luisa", "Loma Linda", "Rohrmoser", "Colón", "Rancho Luna", "Roma", "Tovar"
                             ]
                         },
                         {
                             nombre:"Pavas",
                             barrios:
                             [
-                                
+                                "Alfa", "Asturias", "Asunción", "Aeropuerto", "Bribri", "Favorita Norte", "Favorita Sur", "Galicia", "Geroma", "Hispana", "Libertad", "Lomas del Río", "Llanos de Sol", "María Reina", "Metrópolis", "Navarra", "Pavas Centro", "Pueblo Nuevo", "Residencial del Oeste", "Rincón Grande", "Rohrmoser", "Rotonda", "San Juan", "San Pedro", "Santa Bárbara", "Santa Catalina", "Santa Fe", "Triángulo", "Villa Esperanza", "Zona Industrial"
                             ]
                         },
                         {
                             nombre:"Hatillo",
                             barrios:
                             [
-                                
+                                "Cañada","Belgrano","Hatillo Centro" ,"Hatillo 1" ,"Hatillo 2" ,"Hatillo 3", "Hatillo 4","Hatillo 5","Hatillo 6","Hatillo 7","Hatillo 8","15 de Setiembre","Sagrada Familia","Tiribí","Topacio","25 de Julio","Vivienda en Marcha"
                             ]
                         },
                         {
                             nombre:"San Sebastián",
                             barrios:
                             [
-                                
+                                "Bajo Cañada", "Bengala", "Bilbao", "Camelias", "Cañada Sur", "Carmen", "Cascajal", "Cerro Azul", "Colombari", "Colonia Kennedy", "Domingo Savio", "Guacayama", "Hogar Propio", "Jazmín", "López Mateo", "Luna Park", "Magnolias", "Mojados", "Monguito", "Monte Azul", "Musmani", "Paso Ancho", "Presidentes", "San Martín", "San Sebastián", "Santa Rosa", "Seminario", "Zorobarú"
                             ]
                         },
                     ]
@@ -107,14 +107,48 @@ const PROVINCIAS=
                     distritos:
                     [
                         {
-                            nombre:"",
+                            nombre:"Escazú",
                             barrios:
                             [
-    
+                                "Bello Horizonte (parte)", "Carrizal", "Centro", "El Poró", "La Rosa Linda"
+                            ]
+                        },
+                        {
+                            nombre:"San Antonio",
+                            barrios:
+                            [
+                                "Bebedero", "Bello Horizonte (parte)", "Centro", "El Carmen", "La Nuez", "Palo Campana", "Santa Teresa", "Tejarcillos", "Torrotillo", "Vista de Oro"
+                            ]
+                        },
+                        {
+                            nombre:"San Rafal",
+                            barrios:
+                            [
+                                "Anonos", "Bello Horizonte (parte)", "Centro", "Guachipelín", "Honduras", "Los Laureles", "Maynard", "Palma de Mallorca", "Santa Marta", "Tena", "Trejos Montealegre", "Villa Real (parte)", "Vista Alegre"
                             ]
                         },
                     ]
                 },
+                {
+                    nombre:"Desamparados",
+                    distritos:
+                    [
+                        {
+                            nombre:"Desamparados",
+                            barrios:
+                            [
+                                "Altamira","Calle Angulo","Calle Araya","Calle Fallas","Cocorí","Colonia del Sur","Contadores","Cucubres","Damas","Desamparados (centro)","El Jardín","Kamaquiri (comparte con San José)","Kiria","La Fortuna (comparte con San Antonio)","La Paz","Las Ardillas","Las Brisas","Lomas","Los Cisnes","Los Duraznos","Los Murillo","Los Naranjos","Loto San Jerónimo","Madeira","Marshall","Metrópoli","Monseñor Sanabria","Monte Claro","Río Jorco","Sabara","San Roque","Santa Cecilia","Pinar del Río","Tauro","Tica Linda","Torremolinos","Venecia","Vista Verde"
+                            ]
+                        },
+                        {
+                            nombre:"",
+                            barrios:
+                            [
+
+                            ]
+                        },
+                    ]
+                }
             ]
         },
         {
